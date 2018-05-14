ce0b491e54822f254504d0039b2926e83abaf67a
Rafi Khan <zetoslab@gmail.com>
2018-05-13 21:53:41 -0400

    working on user interface to display and install lessons

--- screens.py
+++ screens.py
@@ -1,3 +1,8 @@
+from functools import partial
+from lib import install
+
+echo = partial(print, end='', flush=True)
+
 class Screen():
     def __init__(self, screen_manager):
         self.screen_manager = screen_manager
@@ -18,7 +23,9 @@ class Screen():
     def render(self, term):
         pass
 
-    def _input(self, key):
+    def _input(self, key, term=None):
+        key = key.lower()
+
         if key in self.input_handlers:
             self.input_handlers[key]()
             return True
@@ -26,8 +33,8 @@ class Screen():
     def handle_input(self, term):
         with term.cbreak():
             while not self.quit:
-                key = term.inkey().lower()
-                self._input(key);
+                key = term.inkey()
+                self._input(key, term);
 
 
 class WelcomeScreen(Screen):
@@ -45,8 +52,7 @@ class WelcomeScreen(Screen):
     {term.bold}[Q]{term.normal}uit
 """
 
-
-        print(render_string)
+        echo(render_string)
 
     def register_input_handlers(self):
         Screen.register_input_handlers(self)
@@ -54,9 +60,24 @@ class WelcomeScreen(Screen):
         def _install():
             ps = PromptScreen("Enter the git repository", self.screen_manager)
             self.screen_manager.next_screen(ps)
+
+        def _begin():
+            lst = []
+
+            elem = {
+                'string': 'Hello',
+                'action': None
+            }
+
+            lst.append(elem)
+            lst.append(elem)
+            
+            bs = ListSelectScreen(lst, self.screen_manager)
+            self.screen_manager.next_screen(bs)
             
        
         self.input_handlers['i'] = _install
+        self.input_handlers['b'] = _begin
 
 class PromptScreen(Screen):
     def __init__(self, prompt, screen_manager):
@@ -66,14 +87,57 @@ class PromptScreen(Screen):
             raise Exception("Missing arguments")
 
         self.prompt = prompt
+        self.input_log = ""
+
+    def render(self, term=None):
+        render_string = f"{self.prompt}: {self.input_log}"
+
+        echo(render_string)
+
+    def install(self):
+        install(self.input_log)
+
+    def _input(self, key, term):
+        if not Screen._input(self, key):
+
+            if key.name == 'KEY_DELETE':
+                self.input_log = self.input_log[:-1]
+            elif key.name == 'KEY_ENTER':
+                self.install()
+            else:
+                self.input_log += key
+
+            self.screen_manager.render()
+
+class ListSelectScreen(Screen):
+    def __init__(self, lst, screen_manager):
+        Screen.__init__(self, screen_manager)
+
+        if not lst or len(lst) < 0:
+            raise Exception("Please check lst")
+
+        self.lst = lst
+        self.selected = 0
+        self.length = len(lst)
 
     def render(self, term):
-        render_string = f"{self.prompt}: "
+        for idx, val in enumerate(self.lst):
+            if idx == self.selected:
+                echo(term.standout)
+                print(val['string'])
+                echo(term.no_standout)
+            else:
+                print(val['string'])
 
-        print(render_string)
-
-    def _input(self, key):
+    def _input(self, key, term):
         if not Screen._input(self, key):
-            print(key, end="", flush=True)
-        
-        
+
+            if key.name == 'KEY_DOWN':
+                self.selected = min(self.selected + 1, self.length)
+            elif key.name == 'KEY_UP':
+                self.selected = max(self.selected - 1, 0)
+            elif key.name == 'KEY_ENTER':
+                from remote_pdb import RemotePdb
+                RemotePdb('localhost', 4444).set_trace()
+
+            self.screen_manager.render()
